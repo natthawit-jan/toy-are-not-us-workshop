bed27cbe89f056f9a6d7290f6be7c9328d02edf2
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-07-30 15:50:20 +0700

    done with toy detail

--- frontend/toy-r-not-us-group5/src/app/app-routing.module.ts
+++ frontend/toy-r-not-us-group5/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ToyDetailComponent } from './toy-detail/toy-detail.component';
+import { ToysListComponent } from './toys-list/toys-list.component';
 
 const routes: Routes = [
+  { path: '', component: ToysListComponent },
   {
     path: 'toy-detail/:id',
     component: ToyDetailComponent,
--- frontend/toy-r-not-us-group5/src/app/toy-detail/toy-detail.component.ts
+++ frontend/toy-r-not-us-group5/src/app/toy-detail/toy-detail.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-toy-detail',
   templateUrl: './toy-detail.component.html',
-  styleUrls: ['./toy-detail.component.css']
+  styleUrls: ['./toy-detail.component.css'],
 })
 export class ToyDetailComponent implements OnInit {
+  toy = {
+    id: 15,
+    name: 'sailboat',
+    gender: 'male',
+    age: 9,
+    brand: 'CoolKidz',
+    price: 24.95,
+    avaliable: true,
+  };
+  quantityForm: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.quantityForm = this.fb.group({
+      quantity: [''],
+    });
   }
 
+  ngOnInit(): void {}
 }
