3023cb32cbd4f2a941aa5c12283fb421a1e16085
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-08-07 21:22:08 +0700

    add backend

---
+++ backend/src/main/java/com/group5/toyrnotus/ToyrnotusApplication.java
@@ -0,0 +1,13 @@
+package com.group5.toyrnotus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyrnotusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyrnotusApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/group5/toyrnotus/ToyrnotusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.toyrnotus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyrnotusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
